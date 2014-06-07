a2e12ecc455666b2414274c684078b7327e28da5
Ramindu Deshapriya <rasade88@gmail.com>
2014-06-07 12:49:49 +0530

    Added new UI from Kasun

---
+++ new_ui/Preparedenss.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>DMU Sahana</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Software " content="Sahana for the Disaster Management Unit, Ministry of Health, Sri Lanka" />
+<meta name="Website" content="http://pl.nlm.nih.gov" />
+<meta name="Licence" content="Lesser General Public Licence, Version 2.1" />
+<meta name="Licence Website" content="http://www.gnu.org/licenses/lgpl-2.1.txt" />
+<meta name="google-site-verification" content="Y2Ts00HnBQEr3M3KegrKRRAMVuQPejmqeqPKDsMGRGw" />
+<link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/fbnpmpdcnjkhfcgeeklebjmopaheplce">
+<link rel="stylesheet" media="screen, projection" type="text/css" href="theme/lpf3/sahana.css" />
+<link rel="stylesheet" media="print" type="text/css" href="theme/lpf3/print.css" />
+<link rel="stylesheet" media="handheld" type="text/css" href="theme/lpf3/mobile.css" />
+<!--[if IE 6]>
+<link rel="stylesheet" type="text/css" href="theme/lpf3/ie6.css" />
+<![endif]-->
+<!--[if IE 7]>
+<link rel="stylesheet" type="text/css" href="theme/lpf3/ie7.css" />
+<![endif]-->
+<!--[if IE 8]>
+<link rel="stylesheet" type="text/css" href="theme/lpf3/ie8.css" />
+<![endif]-->
+<!--[if IE 9]>
+<link rel="stylesheet" type="text/css" href="theme/lpf3/ie9.css" />
+<![endif]-->
+
+<link rel="icon" type="image/png" href="favicon.png">
+
+
+<script type="text/javascript" src="res/js/vesuvius.js"></script>
+<script type="text/javascript" src="index.php?stream=text&amp;mod=xst&amp;act=help"></script>
+
+</head>
+<body><div id="container"><div id="header" class="clearfix"><div id="leftHeaderLogo"><a href="http://localhost/public_html/sahana-dmu/www/"><img id="leftHeaderLogoImg" src="theme/lpf3/img/moh.jpg" alt="Ministry of Health log"></a></div><div id="rightHeaderLogo"><a href="http://www.launchpad.net/vesuvius"><img src="theme/lpf3/img/vesuvius.png" alt="Sahana Vesuvius Logo"></a></div></div>
+			<div id="headerText">
+				<h1>DMU Vesuvius for the Test Exercise</h1>
+				<h3>of January 1, 2000</h3>
+				<h4>Disaster Management Unit</h4>
+				<h4>Ministry of Health, Sri Lanka</h4>
+			</div>
+		<div id="wrapper" class="clearfix"><div id="wrapper_menu">
+		<div id="menuwrap">
+		<ul id="menu">
+	<li ><a href="arrivals">Arrivals</a></li><li ><a href="search">Search for a Person</a></li><li ><a href="report">Report a Person</a></li><li ><a href="tracking">People I'm Tracking</a></li><li ><a href="resources">Help and Resources</a></li><li ><a href="admin">Administration</a></li><li ><a href="outbound">Outbound Email <span style="color: red;"><b>●</b></span></a></li><li ><a href="inbound">Inbound Email <span style="color: red;"><b>●</b></span></a></li><li ><a href="events">Event Manager</a></li><li ><a href="http://localhost/public_html/sahana-dmu/www/">Home</a></li><li ><a href="http://localhost/public_html/sahana-dmu/www/">Preparedenss</a></li>
+		</ul>
+		</div> <!-- /menuwrap -->
+	
+		<div id="loginform">
+			<a href="settings">My Settings</a> <span style="color: #e5eaef;">&nbsp;|&nbsp;</span> <a href="logout">Logout</a>
+		</div>
+	</div><!-- Left hand side menus & login form --><div id="content" class="clearfix">
+		<ul id="tabmenu">
+	<li id='active' ><a href="pref?session&tabid=0"><span>Session Information</span></a></li><li  ><a href="pref?password&tabid=1"><span>Change My Password</span></a></li>
+		</ul>
+	<br><div id="home">
+		<div id="rezMain" class="mainArea" style="opacity: 1; width: 300px;">
+			<table class="emTable">
+				<tbody>
+				<tr>
+					<td style="padding-left: 8px; text-align: left; font-weight: bold;" colspan=2 class="mainRowOdd">Currently Logged in as...</td>
+				</tr>
+				<tr>
+					<td style="padding-left: 8px; text-align: right;" class="mainRowEven">Username</td>
+					<td style="padding-left: 8px; text-align: left; font-weight: bold;" class="mainRowEven">root</td>
+				</tr>
+				<tr>
+					<td style="padding-left: 8px; text-align: right;" class="mainRowOdd">Full Name</td>
+					<td style="padding-left: 8px; text-align: left; font-weight: bold;" class="mainRowOdd">Root /</td>
+				</tr>
+				<tr>
+					<td style="padding-left: 8px; text-align: right;" class="mainRowEven">User Group</td>
+					<td style="padding-left: 8px; text-align: left; font-weight: bold;" class="mainRowEven">Administrator</td>
+				</tr>
+				<tr>
+					<td style="padding-left: 8px; text-align: right;" class="mainRowOdd">Locale</td>
+					<td style="padding-left: 8px; text-align: left; font-weight: bold;" class="mainRowOdd">en_US</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+	</div></div><!-- /content --></div><!-- /wrapper --><div id="blueBack">&nbsp;</div>
+<div id="footer"><center><table id="footerTable">
+		<tr>
+		<td>
+			<a href="http://www.nlm.nih.gov"><img src="theme/lpf3/img/nlm.png"></a>
+			<a href="http://www.nih.gov"><img src="theme/lpf3/img/nih.png"></a>
+			<a href="http://www.hhs.gov"><img src="theme/lpf3/img/hhs.png"></a><br>
+			Based on the People Locator developed<br>
+			by the US National Library of Medicine
+		</td>
+		<td>
+			<a href="http://www.virtusa.com/"><img src="theme/lpf3/img/virtusa_logo.jpg"></a>
+		<br>
+			A CSR project by Virtusa (Pvt) Ltd <br>
+			In corroboration with Virtusa Healthcare policies
+			
+		</td>
+		<td>
+			<a href="http://sahanafoundation.org"><img src="theme/lpf3/img/sahana.png"></a><br>
+			Powered by<br>
+			Sahana Vesuvius
+		</td>
+		</tr>
+	</table>
+	<div id="notice"></div>
+	</center>
+	</div>
+</div><!-- /container --></body></html>
